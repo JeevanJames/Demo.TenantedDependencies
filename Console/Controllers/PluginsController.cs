fd57aa813fc6263f83cfbe5a4c51d8d910502998
Jeevan James <jeevanjj@live.com>
2020-04-19 19:26:07 +0530

    Add project files.

---
+++ Console/Controllers/PluginsController.cs
@@ -0,0 +1,17 @@
+﻿using Console.Code;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Console.Controllers
+{
+    [ApiController]
+    [Route("plugins")]
+    public class PluginsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetValue([FromServices]PluginDependency<IMyService> myServicePlugin)
+        {
+            IMyService myService = myServicePlugin.Get();
+            return myService.GetValue();
+        }
+    }
+}
